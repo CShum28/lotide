5938cf8b77de2baf2de872466120f7831f904ee2
Clement Shum <clement.shum128@gmail.com>
2023-01-25 21:31:58 -0500

    Comparing two arguements, one of the arguements is an array

---
+++ head.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// const assertEqual = function(actual, expected) {
+//   console.assert(actual === expected, 'this is not equal');
+// };
+
+const head = function(items) {
+  return items[0];
+}
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(36, 'hi');
+
+assertEqual(head([8,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
