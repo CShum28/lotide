38e8eff2843c49aad8f0d8425b108ec266069805
Clement Shum <clement.shum128@gmail.com>
2023-01-26 21:55:09 -0500

    lotide implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// const assertEqual = function(actual, expected) {
+//   console.assert(actual === expected, 'this is not equal');
+// };
+
+const eqArrays = function (inputOne, inputTwo) {
+  if (inputOne.length !== inputTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < inputOne.length; i++) {
+    if (inputOne[i] !== inputTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
