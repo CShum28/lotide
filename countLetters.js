42afaca97e9511dfae1d18a25b53f3027fc18650
Clement Shum <clement.shum128@gmail.com>
2023-01-29 16:08:33 -0500

    counterLetter updated code - used
    'in' operator

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (sentence) => {
+  sentence = sentence.replace(/ /g, "");
+  let output = {};
+  for (const letter of sentence) {
+    if (!(letter in output)) {
+      // if (!output[letter]) { - this works as well
+      output[letter] = 1;
+    } else {
+      output[letter] += 1;
+    }
+  }
+  return output;
+};
+
+// TEST CODE
+console.log(countLetters("lighthouse in the house"));
